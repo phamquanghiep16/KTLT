3c53051479c02e9e7d9f70464b1e90ef5ce34dae
phamquanghiep16 <pqhh1605@gmail.com>
2025-05-23 09:06:31 +0700

    Delete baif 2.7.py

--- baif 2.7.py
+++
@@ -1,8 +0,0 @@
-# Yêu cầu người dùng nhập số nguyên n
-n = int(input("Nhập số nguyên n: "))
-
-# Tạo dictionary với các cặp (i, i*i)
-squares_dict = {i: i*i for i in range(1, n+1)}
-
-# In ra dictionary
-print(squares_dict)
